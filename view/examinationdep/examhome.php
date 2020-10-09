9bf42757a750023c779dd5fa916ff04c78748b2c
ShakyaAbeytunge <56604646+ShakyaAbeytunge@users.noreply.github.com>
2020-10-09 14:10:32 +0530

    examination details- examdep

---
+++ view/examinationdep/examhome.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Examination Department Home</title>
+    <link rel="stylesheet" href="../../libs/main.css" type="text/css">
+</head>
+<body>
+    <div class="container">
+        <div class="col-6-item page-title bg-lblue">
+            Examination Department
+        </div>
+    </div>
+    <div class="row container">
+        <div class="col-5-item bg-lblue" style="height: 600px;">
+            <div class="tab">
+                <div class="col-2-item">
+                    <button id="defaulttab" class="tablinks" onclick="openTab(event, 'examinationdetails')" >Examination Details</button>
+                </div>
+                <div class="col-2-item">
+                    <button class="tablinks" onclick="openTab(event, 'uploadpastpapers')">Upload Past Papers</button>
+                </div>
+                <div class="col-2-item">
+                    <button class="tablinks" onclick="openTab(event, 'managesubjects')">Manage Subjects</button>
+                </div>
+            </div>
+
+            <div id="examinationdetails" class="tabcontent" style="height: 500px;">
+                <div class="col-2-item">
+                    <div class="col-2-item strong text-white">Year of Examination:</div>
+                    <div class="col-4-item">
+                        <input type="text" name="year" placeholder="<?php echo date("Y"); ?>">
+                    </div>
+                </div>
+                <div class="col-2-item">
+                    <div class="col-2-item strong text-white">Select Semester:</div>
+                    <div class="col-4-item">
+                        <input type="text" list="semester" name="semester">
+                        <datalist id="semester">
+                            <option value="Semester-I">
+                            <option value="Semester-II">
+                        </datalist>
+                    </div> 
+                </div>
+                <div class="col-2-item">
+                    <div class="col-2-item strong text-white">Select Year of Study:</div>
+                    <div class="col-4-item">
+                        <input type="text" list="studyyear" name="studyyear">
+                        <datalist id="studyyear">
+                            <option value="First Year">
+                            <option value="Second Year">
+                            <option value="Third Year">
+                            <option value="Forth Year">
+                        </datalist>
+                    </div>
+                </div>                    
+                    
+                
+                <div class="row">
+                    <div class="col-3-item">
+                        <div class="col-2-item strong text-white">Select Subject:</div>
+                        <div class="col-4-item">
+                            <input type="text" list="subject" name="subject">
+                            <datalist id="subject">
+                                <option value="Subject-I">
+                                <option value="Subject-II">
+                                <option value="Subject-III">
+                                <option value="Subject-IV">
+                            </datalist>
+                        </div>
+                    </div>
+                    <div class="col-2-item">
+                        <div class="col-4-item strong text-white">Choose Exam Date:</div>
+                        </br>
+                        <div class="date-picker">
+                            <div class="selected-date"></div>
+                            <div class="dates">
+                                <div class="month">
+                                    <div class="arrows prev-mth">&lt;</div>
+                                    <div class="mth"></div>
+                                    <div class="arrows next-mth">&gt;</div>
+                                </div>
+                                <div class="days"></div>
+                            </div>
+                        </div>                    
+                    </div>
+                    <div class="col-1-item">
+                    </br></br>
+                        <button class="bg-blue border-blue" type="submit">Add to Schedule</button>
+                    </div>
+                </div>
+                <div class="container-fit-vertical bg-white text-gray auto-scroll" style="height: 200px; width: 50%">&lt;Schedule&gt;</div>                         
+            </div>
+            
+            <div id="uploadpastpapers" class="tabcontent" style="height: 500px;">
+                <div class="col-5-item">
+                    <div class="col-2-item strong text-white">Enter Post ID:</div>
+                    <div class="col-4-item"><input type="text" placeholder="Post ID" id="postid"></div>
+                    <div class="col-2-item strong text-white">Content:</div>
+                    <div class="col-4-item bg-white text-gray" style="height: 300px;">&lt;Display Content Here&gt;</div>
+                    <div class="col-4-item" style="float: right;">
+                        <div class="col-3-item"><button class="bg-green border-green" type="submit">Ignore Repot & Keep the Post</button></div>
+                        <div class="col-3-item"><button class="bg-red border-red" type="submit">Remove Post</button></div>
+                    </div>
+                </div>
+                <div class="col-1-item">
+                    <div class="col-4-item"><button class="bg-dblue border-dblue" type="submit">Search Post</button></div>
+                    
+                </div>
+            </div>
+
+            <div id="managesubjects" class="tabcontent" style="height: 500px;">
+                <div class="col-5-item">
+                    <div class="col-2-item strong text-white">Enter Post ID:</div>
+                    <div class="col-4-item"><input type="text" placeholder="Post ID" id="postid"></div>
+                    <div class="col-2-item strong text-white">Content:</div>
+                    <div class="col-4-item bg-white text-gray" style="height: 300px;">&lt;Display Content Here&gt;</div>
+                    <div class="col-4-item" style="float: right;">
+                        <div class="col-3-item"><button class="bg-green border-green" type="submit">Ignore Repot & Keep the Post</button></div>
+                        <div class="col-3-item"><button class="bg-red border-red" type="submit">Remove Post</button></div>
+                    </div>
+                </div>
+                <div class="col-1-item">
+                    <div class="col-4-item"><button class="bg-dblue border-dblue" type="submit">Search Post</button></div>
+                    
+                </div>
+            </div>
+        </div>
+        <div class="col-1-item bg-gray" style="height: 600px;">
+            <div class="container-fit-vertical bg-lgray strong">
+                Notifications
+            </div>
+            <div class="container-fit-vertical bg-white text-gray auto-scroll" style="height: 520px;">
+                &lt;Notification list&gt;
+            </div>
+        </div>
+    </div>
+
+    <script src="../../libs/main.js"></script>
+</body>
+</html>
