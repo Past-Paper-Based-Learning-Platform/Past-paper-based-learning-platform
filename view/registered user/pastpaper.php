d52e5c0e124116b33690a48d8e894d01302746e3
ChamodhiSatharasinghe <56638407+ChamodhiSatharasinghe@users.noreply.github.com>
2021-03-30 22:55:23 +0530

    logo changes

--- view/registered user/pastpaper.php
+++ view/registered user/pastpaper.php
@@ -9,7 +9,6 @@
 <body>
     <section class = 'logohead'>
         <a href='http://localhost/Main/homeindex.php?page=home.php'><img src= 'pictures/logoPPB.png' class='logoimg'></a>
-        <h1 class="sitename">Past Paper Base Learning PlatForm</h1>
     </section>
     <section class="pdf">
         <div class="sidetab">
--- view/registered user/profilesetting.php
+++ view/registered user/profilesetting.php
@@ -9,7 +9,6 @@
 
 <section class = 'logohead'>
   <a href='http://localhost/Main/homeindex.php?page=home.php'><img src= 'pictures/logoPPB.png' class='logoimg'></a>
-  <h1 class="sitename">Past Paper Base Learning PlatForm</h1>
 </section>
 
 <h2 class="navigation">Profile Setting</h2>
